876a1580ccc3e0719803e3f72c9ec49f8f210894
Manohar- <manoharsai0275@gmail.com>
2023-08-14 19:22:10 +0530

    User-1 changes done

--- Application.java
+++ Application.java
@@ -15,4 +15,30 @@ public class Application {
         
         scanner.close();
     }
+#Use-1 changes done by manohar on 14-08-2023
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Application {
+    public static void main(String[] args) {
+        try {
+            FileReader reader = new FileReader("input.txt");
+            FileWriter writer = new FileWriter("output.txt");
+            
+            int character;
+            while ((character = reader.read()) != -1) {
+                writer.write(character);
+            }
+            
+            reader.close();
+            writer.close();
+            
+            System.out.println("File copied successfully.");
+        } catch (IOException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
 }
+
+
